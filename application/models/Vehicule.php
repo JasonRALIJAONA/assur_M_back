4928ec6ad8e03da68d984e798ef0fbbab067ed30
mihary563 <miharitianar.ramiandrisoa@gmail.com>
2024-06-01 12:43:01 +0300

    crud_vehicule

---
+++ application/models/Vehicule.php
@@ -0,0 +1,39 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Vehicule extends CI_Model {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
+    
+
+    public function get_all(){
+        $query = $this->db->get('vehicule');
+        return $query->result_array();
+    }
+
+    /*
+    format de $data :
+        $data = array(
+            'immatriculation' => 'XX-XXXX-XX',
+            'puissance' => 150,
+            'marque' => 'BMW',
+            autre colonne ...
+        );
+    */
+
+    public function insert($data){
+        return $this->db->insert('vehicule', $data);
+    }
+
+    public function update($id, $data){
+        $this->db->where('id', $id);
+        return $this->db->update('vehicule', $data);
+    }
+
+    public function delete($id){
+        return $this->db->delete('vehicule', array('id' => $id));
+    }
+}
