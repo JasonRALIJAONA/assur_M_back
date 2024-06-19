49077d0626dbc634fe500e2a75dbc1c721be2426
CedtT198 <cedrictiavina426@gmail.com>
2024-06-19 07:47:48 -0600

    update stat

--- application/models/statistique.php
+++ application/models/statistique.php
@@ -4,15 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class statistique extends CI_Model {
     
     public function get_assurance_plus_utilise($mois, $annee) {
-        $query = "SELECT f.id_assureur,
+        $query = "SELECT f.id_assureur, a.nom, 
         COUNT(DISTINCT v.id_utilisateur) AS nombre_utilisateurs
         FROM facture f
+        JOIN assureur a ON f.id_assureur = a.id
         JOIN vehicule v ON f.id_vehicule = v.id
         JOIN utilisateur u ON v.id_utilisateur = u.id
-        WHERE EXTRACT(YEAR FROM f.date_debut) = ?
-        AND EXTRACT(MONTH FROM f.date_debut) = ?
-        GROUP BY annee, mois, f.id_assureur
-        ORDER BY annee, mois, f.id_assureur";
+        WHERE EXTRACT(YEAR FROM f.date_debut) = 2023
+        AND EXTRACT(MONTH FROM f.date_debut) = 1
+        GROUP BY a.nom, f.id_assureur
+        ORDER BY a.nom, f.id_assureur";
 
         $query = $this->db->query($query, array($annee, $mois));
         return $query->result_array();
