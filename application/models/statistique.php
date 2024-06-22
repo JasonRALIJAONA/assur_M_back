80aeb9c756f8677dd432cc75c5d78a13d0558d83
JoharyManantena <166318905+JoharyManantena@users.noreply.github.com>
2024-06-22 20:12:14 +0300

    testMerge

--- application/models/statistique.php
+++ application/models/statistique.php
@@ -55,20 +55,6 @@ class statistique extends CI_Model {
         return $query->result_array();
     }
     
-    public function get_assurance_plus_utilise($mois, $annee) {
-        $query = "SELECT f.id_assureur,
-        COUNT(DISTINCT v.id_utilisateur) AS nombre_utilisateurs
-        FROM facture f
-        JOIN vehicule v ON f.id_vehicule = v.id
-        JOIN utilisateur u ON v.id_utilisateur = u.id
-        WHERE EXTRACT(YEAR FROM f.date_debut) = ?
-        AND EXTRACT(MONTH FROM f.date_debut) = ?
-        GROUP BY annee, mois, f.id_assureur
-        ORDER BY annee, mois, f.id_assureur";
-
-        $query = $this->db->query($query, array($annee, $mois));
-        return $query->result_array();
-    }
 
     public function get_frequence_payement($mois, $annee) {
         $query = "SELECT 
