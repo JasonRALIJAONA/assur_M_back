837f988cb45d4f69f10afda674cd1fa069dab414
JoharyManantena <166318905+JoharyManantena@users.noreply.github.com>
2024-06-22 20:20:11 +0300

    oui

---
+++ application/logs/log-2024-06-22.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-06-22 19:18:36 --> 404 Page Not Found: RechercheCtrl/search
+ERROR - 2024-06-22 19:18:38 --> 404 Page Not Found: RechercheCtrl/search
+ERROR - 2024-06-22 19:18:39 --> 404 Page Not Found: RechercheCtrl/search
+ERROR - 2024-06-22 19:18:42 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:42 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:44 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:44 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:46 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:46 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:47 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:47 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:48 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:48 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:49 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:49 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:50 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:50 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-22 19:18:56 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-22 19:18:56 --> 404 Page Not Found: Assets/vendors
