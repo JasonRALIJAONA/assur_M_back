62797a4c806c243249966a96e23df4e665b7f639
JoharyManantena <166318905+JoharyManantena@users.noreply.github.com>
2024-06-26 11:39:37 +0300

    VITA !

---
+++ application/logs/log-2024-06-26.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-06-26 10:25:07 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-26 10:25:07 --> 404 Page Not Found: Assets/js
+ERROR - 2024-06-26 10:25:14 --> 404 Page Not Found: Assets/vendors
+ERROR - 2024-06-26 10:25:14 --> 404 Page Not Found: Assets/js
